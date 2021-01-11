c35af240ad952f7345c0126b243ea6b1930fe9e7
lvpangpang <574818075@qq.com>
2021-01-11 16:48:16 +0800

    test1

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 function App() {
-  return <div className="dabc">哈哈</div>
+  return <div className="dabc">哈哈123</div>
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
